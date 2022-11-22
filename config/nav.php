21d7515cd91a59c1ab353ae34548b09b68bea9c9
MagedGhabour <92618945+Maged-Ghabour@users.noreply.github.com>
2022-11-22 18:01:39 +0200

    DashboardComponent

--- config/nav.php
+++ config/nav.php
@@ -22,44 +22,44 @@ return [
         "route2" => 'dashboard.products.create',
 
     ],
-    // [
-    //     'icon' =>  'nav-icon fas fa-users' ,
-    //     'route'=>  '#'   ,
-    //     "head"=>   ' المستخدمين',
-    //     "title1"=>  'جميع المستخدمين',
-    //     "route1"=>  '#',
-    //     "title2" => 'إضافة مستخدم جديد',
-    //     "route2" => '#',
-    // ],
-    // [
-    //     'icon' =>  'nav-icon fas fa-gift' ,
-    //     'route'=>  '#'   ,
-    //     "head"=>   ' العروض',
-    //     "title1"=>  'جميع العروض',
-    //     "route1"=>  '#',
-    //     "title2" => 'إضافة عرض جديد',
-    //     "route2" => '#',
+    [
+        'icon' =>  'nav-icon fas fa-users' ,
+        'route'=>  'dashboard.users.index'   ,
+        "head"=>   ' المستخدمين',
+        "title1"=>  'جميع المستخدمين',
+        "route1"=>  'dashboard.users.index',
+        "title2" => 'إضافة مستخدم جديد',
+        "route2" => 'dashboard.users.create',
+    ],
+    [
+        'icon' =>  'nav-icon fas fa-gift' ,
+        'route'=>  'dashboard.offers.index'   ,
+        "head"=>   ' العروض',
+        "title1"=>  'جميع العروض',
+        "route1"=>  'dashboard.offers.index',
+        "title2" => 'إضافة عرض جديد',
+        "route2" => 'dashboard.offers.create',
 
-    // ],
-    // [
-    //     'icon' =>  'nav-icon fas fa-hourglass' ,
-    //     'route'=>  '#'   ,
-    //     "head"=>   ' الوظائف',
-    //     "title1"=>  'جميع الوظائف',
-    //     "route1"=>  '#',
-    //     "title2" => 'إضافة وظيفة جديد',
-    //     "route2" => '#',
+    ],
+    [
+        'icon' =>  'nav-icon fas fa-hourglass' ,
+        'route'=>  'dashboard.jobs.index'   ,
+        "head"=>   ' الوظائف',
+        "title1"=>  'جميع الوظائف',
+        "route1"=>  'dashboard.jobs.index',
+        "title2" => 'إضافة وظيفة جديد',
+        "route2" => 'dashboard.jobs.create',
 
-    // ],
-    // [
-    //     'icon' =>  'nav-icon fas fa-user-plus' ,
-    //     'route'=>  '#'   ,
-    //     "head"=>   ' عملاؤنا',
-    //     "title1"=>  'جميع العملاء',
-    //     "route1"=>  '#',
-    //     "title2" => 'إضافة عميل جديد',
-    //     "route2" => '#',
-    // ]
+    ],
+    [
+        'icon' =>  'nav-icon fas fa-user-plus' ,
+        'route'=>  'dashboard.ourCustomers.index'   ,
+        "head"=>   ' عملاؤنا',
+        "title1"=>  'جميع العملاء',
+        "route1"=>  'dashboard.ourCustomers.index',
+        "title2" => 'إضافة عميل جديد',
+        "route2" => 'dashboard.ourCustomers.create',
+    ]
 
 ];
 
---
+++ resources/views/Dashboard/Jobs/create.blade.php
@@ -0,0 +1,23 @@
+@extends("layouts.dashboard")
+
+@section("title" , "إضافة تصنيف جديد")
+
+
+
+    @section("breadcrump")
+    @parent
+        <li class="breadcrumb-item"><a href="{{route('dashboard.categories.index')}}">التصنيفات</a></li>
+        <li class="breadcrumb-item"><a href="{{route('dashboard.categories.create')}}">إضافة تصنيف جديد</a></li>
+    @endsection
+
+
+
+
+
+@section("content")
+<form action="{{route('dashboard.categories.store')}}" method="post" enctype="multipart/form-data">
+    @csrf
+    @include("Dashboard.Categories._from")
+</form>
+
+@endsection
