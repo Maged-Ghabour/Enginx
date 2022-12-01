92fb38b97bba7bcf20380e8fee362a93220302a2
KarimZakaria <54362523+KarimZakaria@users.noreply.github.com>
2022-12-02 01:05:43 +0200

    Creating Order For Customers

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -1,18 +1,21 @@
 <?php
 
 namespace App\Http\Controllers\Dashboard;
+
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Customer;
 use App\Models\Job;
 use App\Models\Offer;
+use App\Models\Order;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $catsCount = Category::count();
         $productsCount = Product::count();
@@ -20,6 +23,7 @@ class DashboardController extends Controller
         $customersCount = Customer::count();
         $jobsCount = Job::count();
         $usersCount = User::count();
-        return view("Dashboard.index" , compact("catsCount" , "productsCount" , "offersCount" , "customersCount" ,"jobsCount" ,"usersCount"));
+        $orders = Order::count();
+        return view("Dashboard.index", compact("catsCount", "productsCount", "offersCount", "customersCount", "jobsCount", "usersCount", "orders"));
     }
 }
